588099d6e31f36c85adc82788f3bec6b42ef87de
Kenkooo <69892433+Kenkooo@users.noreply.github.com>
2022-08-31 10:57:08 +0900

    ChangeExecutionPolicy.ps1を追加

---
+++ ChangeExecutionPolicy.ps1
@@ -0,0 +1,10 @@
+
+
+#一時的に実行ポリシーの設定を変更
+# Restricted:スクリプトの実行禁止
+# AllSigned:署名付きローカルスクリプトのみ実行可能
+# RemoteSigned:ローカルの署名無しとインターネット上の署名付きスクリプトを実行可能
+# Unrestricted:全てのスクリプトを実行可能
+Set-ExecutionPolicy RemoteSigned -Scope Process
+
+
