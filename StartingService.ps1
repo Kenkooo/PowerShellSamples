32eb5a630dcd5314e2c62345a524780a723afcf7
Kenkooo <69892433+Kenkooo@users.noreply.github.com>
2022-09-08 16:47:04 +0900

    Update:StartingService.ps1

--- StartingService.ps1
+++ StartingService.ps1
@@ -10,20 +10,11 @@ if ($ServiceInfo.status -ne "Running") {
     # Write to the console that the services is not running
     Write-Host "Service is not started, starting service"
     Start-Service -Name $ServiceName
-}
-
-echo $ServiceName
-
-
-	## Write to the console that the service is not running
-	Write-Host 'Service is not started, starting service'
-	## Start the service
-	Start-Service -Name $ServiceName
-	## Update the $ServiceInfo object to reflect the new state
-	$ServiceInfo.Refresh()
-	## Write to the console the Status property which indicates the state of the service
+    # Update the $Service Info object to reflect the new state
+    $ServiceInfo.Refresh()
+	# Write to the console the status property which indicates the state  of the service
 	Write-Host $ServiceInfo.Status
-} else { ## If the Status is anything but Running
-	## Write to the console the service is already running
-	Write-Host 'The service is already running.'
+} else {
+	# Write to the console the service is already runnning
+	Write-Host "The service is already running"
 }
