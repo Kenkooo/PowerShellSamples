db7b1accc1b1be0427d04287bc33b82fdf242283
Kenkooo <69892433+Kenkooo@users.noreply.github.com>
2022-09-09 10:35:29 +0900

    Add: DeleteFilesOlderThanXdays.ps1

--- DeleteFilesOlderThanXdays.ps1
+++ DeleteFilesOlderThanXdays.ps1
@@ -1,4 +1,19 @@
 
-$DeleteTime = "7"
+# 日付を取得
+$NowDate = Get-Date -Format "yyyyMMdd_HHmmss"
+# 削除したいファイルの基準時間設定
+$DeleteTime = "-14400"       ### 10譌･
+ 
+ 
+# 対象フォルダのアイテム取得
+Get-ChildItem F:\Lanscope_CSV |
+# 作成時間が$DeleteTimeの値を過ぎたファイルを抽出
+Where-Object {
+$_.CreationTime -lt (Get-Date).AddMinutes($DeleteTime)
+} |
+# 対象ファイルを絶対パスに変換し削除を実行
+foreach {
+    Remove-Item -Recurse -Force $_.FullName
+}
 
-Get-Children -Path 
+exit
